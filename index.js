fa10872027e2274849913ffe734d0283cd454c5f
Deka Louis <83022812+dekalouis@users.noreply.github.com>
2023-09-02 10:43:17 +0700

    Added styling and proper placement of html also added operate function

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-
-let add = function(x, y) {
-    return x+y;
-}
-let subtract = function(x, y) {
-    return x-y;
-}
-let multiply = function(x, y) {
-    return x*y;
-}
-let divide = function(x, y) {
-    return x/y;
-}
+let add = function (x, y) {
+  return x + y;
+};
+let subtract = function (x, y) {
+  return x - y;
+};
+let multiply = function (x, y) {
+  return x * y;
+};
+let divide = function (x, y) {
+  if (y === 0) {
+    return "Error: Division by zero!";
+  }
+  return x / y;
+};
 
 const addBtn = document.getElementById("add");
 const subtractBtn = document.getElementById("subtract");
@@ -21,18 +23,26 @@ let firstNum;
 let secondNum;
 let operator;
 
-
-let operate = function(x, y, operator) {
-    
+function operate(operator, x, y) {
+  switch (operator) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+    default:
+      return "Invalid operator!";
+  }
 }
-
 
 //---------------------------------
 //Testing out button functionality.
-let buttons = [addBtn, subtractBtn, multiplyBtn, divideBtn]
-buttons.forEach(button => {
-    button.addEventListener('click', function() {
-        alert("Hello world")
-    })
+let buttons = [addBtn, subtractBtn, multiplyBtn, divideBtn];
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    alert("Hello world");
+  });
 });
-
